3dd1ef77c8bc6992f89a8a06a3ab9485b910cd06
Mateusz Kowalke <mateusz.kowalke@aurafutures.com>
2021-03-01 19:15:01 +0100

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+function Autobind(_: any, __: string | Symbol, descriptor: PropertyDescriptor) {
+  const origMethod = descriptor.value;
+  const moddedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get () {
+      return origMethod.bind(this);
+    }
+  }
+  return moddedDescriptor;
+}
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+    this.configure();
+    this.attach();
+  }
+
+  @Autobind
+  private submithandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submithandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
